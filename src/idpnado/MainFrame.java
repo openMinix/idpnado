eab26c7aeabccac790b23fecb794d3ffd74dbd30
Vlad Barosan <vlad.barosan@gmail.com>
2014-03-09 16:50:00 +0200

    Dummy gui application

---
+++ src/idpnado/MainFrame.java
@@ -0,0 +1,30 @@
+package idpnado;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class MainFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5058979263127835567L;
+	private JPanel contentPane;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
