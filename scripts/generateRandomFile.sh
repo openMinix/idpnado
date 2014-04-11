31af7ca3606d8de1476c97f1437bc7566fbedde5
Nicolae Ciobanu <nicu.ciobanu91@gmail.com>
2014-04-11 23:13:09 +0300

    script for random file

---
+++ scripts/generateRandomFile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+[[ $# -eq 3 ]] || {
+	echo "Usage : ./generateRandomFile.sh userName fileName size(kb)"
+	exit 1
+}
+
+SCRIPT_PATH="`dirname \"$0\"`"
+SCRIPT_PATH="`(cd "$SCRIPT_PATH" && pwd)`"
+ROOT=$SCRIPT_PATH/../
+
+dd if=/dev/urandom of=$ROOT/runtime/$1/$2 bs=1024 count=$3
